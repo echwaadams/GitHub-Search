a659902fa9ca6b00a46c777065681942d33d7682
echwaadams <adamsechwa18@gmail.com>
2021-10-15 14:53:07 +0300

    add base url to the environment

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private Baseurl = environment.BASE_URL;
 
   private username:string;
   private client_id = '';
