93519c64a6e09e1d9388cf56710f6e5e314dd0ae
echwaadams <adamsechwa18@gmail.com>
2021-10-15 10:49:20 +0300

    added bootstrap

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username:string;
+  private client_id = '';
+  private client_secret = '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';
+
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.client_id + "&client_secret=" + this.client_secret)
+  }
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id="+ this.client_id + "&client_secret=" + this.client_secret)
+  }
+
+  updateProfile(username:string){
+    this.username = username;
+  }
+
+  constructor(private http:HttpClient) {
+    this.username = "echwaadams";
+   }
 }
