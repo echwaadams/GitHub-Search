ba00a93bb4b5fd96723c3067eaf5dfc9103a4d8f
echwaadams <adamsechwa18@gmail.com>
2021-10-17 19:56:12 +0300

    generated a search-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NotFoundComponent } from './not-found/not-found.component'
 import { ProfileService } from './profile/profile.service';
 import { HighlightDirective } from './highlight.directive';
 import { DateCountPipe } from './date-count.pipe';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { DateCountPipe } from './date-count.pipe';
     NotFoundComponent,
     HighlightDirective,
     DateCountPipe,
+    SearchFormComponent,
   ],
   imports: [
     BrowserModule,
